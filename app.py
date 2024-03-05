dec02b41636c454497e4f16de875140105d98a86
Ayush Verma <106526797+CrimsonREwind@users.noreply.github.com>
2024-03-06 03:24:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ with tab1:
 
 
     if st.button("run"):
-        input_a = "/content/gdrive/MyDrive/roop/input/" + image_input
-        input_b = "/content/gdrive/MyDrive/roop/input/" + video_input
-        output = "/content/gdrive/MyDrive/roop/output/" + video_output
-        new_directory = '/content/gdrive/MyDrive/roop'
+        input_a = "/content/roop/input/" + image_input
+        input_b = "/content/roop/input/" + video_input
+        output = "/content/roop/output/" + video_output
+        new_directory = '/content/roop'
         os.chdir(new_directory)
         normal = f'python run.py -s {input_a} -t {input_b} -o {output} --keep-frames --keep-fps --execution-provider cuda --frame-processor face_swapper face_enhancer'
         enhanced = f'python run.py -s {input_a} -t {input_b} -o {output} --keep-frames --keep-fps --execution-provider cuda --frame-processor face_swapper'
